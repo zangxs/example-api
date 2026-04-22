3af2020742056b9d93cb1a996b4d1f69d8eac0b4
brayan <brayanstivenp578@gmail.com>
2026-04-22 15:11:37 -0500

    solucionado bug en el request

--- app/dto/request/weatherRequest.py
+++ app/dto/request/weatherRequest.py
@@ -1,16 +1,16 @@
 from pydantic import BaseModel, field_validator
-from datetime import date
+from datetime import date as date_type, timedelta
 from typing import Optional
 
 class WeatherRequest(BaseModel):
     ciudad: str
-    date: Optional[date] = None
+    date: Optional[date_type] = None
 
     @field_validator("date")
     @classmethod
     def date_not_older_than_10_days(cls, v):
         if v is None:
             return v
-        if v < date.today() - timedelta(days=10):
+        if v < date_type.today() - timedelta(days=10):
             raise ValueError("La fecha no puede ser mayor a 10 días atrás")
         return v
